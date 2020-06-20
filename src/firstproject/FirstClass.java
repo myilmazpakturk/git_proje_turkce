4d390fcd48d948bdc5b27ae7f22c98a32ee86239
myilmazpakturk <67061931+myilmazpakturk@users.noreply.github.com>
2020-06-20 15:44:27 -0400

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstproject/FirstClass.java
+++ src/firstproject/FirstClass.java
@@ -2,7 +2,7 @@ package firstproject;
 
 public class FirstClass {
 	public static void main(String[] args) {
-		System.out.println("merhaba java");
+		System.out.println("i wanna make conflict");
 
 		System.out.println("Hello World");
   
